64e2844b50025095562f82d65bd1b25cb9c08e40
Byron Austin <byronaustin007@gmail.com>
2012-02-22 11:31:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autocare::Application.config.secret_token = 'c549cebc0cd361ee4dbc1f17b9f8dacd02039d967cc7278d0b1c65e68103b3400824c2c7972e6ca7ba0e788826fbbfdd60c2a687ce9284c85f9657951d6905f8'
